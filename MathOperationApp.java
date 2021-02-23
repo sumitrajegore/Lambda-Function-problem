fd059dc615e3ceba0591c0e26d308c89797f2939
sumitrajegore <sumitrajegorevp@gmail.com>
2021-02-23 16:24:44 +0530

    Added passing parameter using lambda function

--- MathOperationApp.java
+++ MathOperationApp.java
@@ -2,6 +2,11 @@
 interface IMathFunction {
 
 	int calculate(int a, int b);
+
+        static void PrintResult(int a, int b, String Function, IMathFunction fobj) {
+    	
+		System.out.println("result of " +Function +" is "+fobj.calculate(a, b));
+	    }
 }
 
 public class MathOperationApp {
@@ -21,6 +26,12 @@ public class MathOperationApp {
 			 System.out.println("subtraction is " +subtract.calculate(6,3));
 			 System.out.println("multiply is " +multiply.calculate(6,3));
 			 System.out.println("division is " +div.calculate(6,3));
+
+                        //passing lambda expression as function parameter using static function
+			IMathFunction.PrintResult(6, 3, "addition", add);
+		     	IMathFunction.PrintResult(6, 3, "division", subtract);
+		     	IMathFunction.PrintResult(6, 3, "addition", multiply);
+		     	IMathFunction.PrintResult(6, 3, "division", div);
     	 	}
  
 }
